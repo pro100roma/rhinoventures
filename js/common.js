919f7a18f08278d5a5e55092bc582edb42404ef7
Roman <r.vorobiov@4service-group.com>
2020-05-24 11:53:30 +0300

    home page done 1

--- js/common.js
+++ js/common.js
@@ -1,6 +1,23 @@
 jQuery(function($) {
 	$(document).ready(function() {
-		
+
+		//Mobile menu
+		$(".toggle_mnu").click(function() {
+			$(".sandwich").toggleClass("active");
+			if ($(".header__menu").hasClass('active')) {
+				$(".header__menu").removeClass('active');
+				document.body.style.overflow = 'visible';
+			} else {
+				$(".header__menu").addClass('active');
+				document.body.style.overflow = 'hidden'; 
+			}
+		});
+		$(".header__menu a").click(function() {
+			$(".header__menu").removeClass('active');
+			$(".sandwich").toggleClass("active");
+			document.body.style.overflow = 'visible';
+		});
+
 	});
 });
 
