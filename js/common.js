3e195a904fd9f37f086ca6589698e6fc238f699e
Roman <r.vorobiov@4service-group.com>
2020-05-31 16:42:37 +0300

    edits 31_05_20

--- js/common.js
+++ js/common.js
@@ -42,7 +42,9 @@ jQuery(function($) {
 			$(this).siblings('p').addClass('active');
 		});
 		$('.input-group input, .input-group textarea').focusout(function() {
-			$(this).siblings('p').removeClass('active');
+			if($(this).val().length == 0) {
+				$(this).siblings('p').removeClass('active');
+			}
 		});
 
 		//Map
@@ -50,5 +52,70 @@ jQuery(function($) {
 			$(this).find('.tumbler').toggleClass('active');
 			$('.singapore-map, .vietnam-map').toggleClass('active');
 		});
+
+		//Validate
+		$("form button[type=submit]").on("click", validate);
+ 
+		// Validate email
+		function validateEmail(email) {
+			let re = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+			return re.test(String(email).toLowerCase());
+		}
+		function validateName(name) {
+			if(name.length < 1 || name.length > 40) {
+				return false;
+			}
+			let nameArr = name.split('');
+			for(let i = 0; i < nameArr.length; i++) {
+				if(nameArr[i] == ' ') {
+					return false;
+				}
+			}
+			return true;
+		}
+		
+		// send form
+		// function sendForm() {
+		// 	$(".alert-error").text("Form sending").fadeIn();
+		// }
+		
+		// validate email and send form after success validation
+		function validate() {
+			let email = $(".email").val(),
+				name = $(".name").val(),
+				mess = $(".message").val(),
+				emailError = $(".input-group__email .alert-error"),
+				nameError = $(".input-group__name .alert-error"),
+				messError = $(".input-group__message .alert-error");
+
+			emailError.text("");
+			nameError.text("");
+			messError.text("");
+		
+			if (validateEmail(email)) {
+				emailError.fadeOut();
+			} else {
+				emailError.fadeIn();
+				emailError.text("Field is not valid");
+				
+			}
+
+			if (validateName(name)) {
+				nameError.fadeOut();
+			} else {
+				nameError.fadeIn();
+				nameError.text("Field is not valid");
+			}
+
+			if(mess.length < 5 || mess.length > 500) {
+				messError.fadeIn();
+				messError.text("Field is not valid");
+				
+			} else {
+				messError.fadeOut();
+			}
+
+			return false;
+		}
 	});	
 });
